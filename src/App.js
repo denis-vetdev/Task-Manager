9b5de9757921f5315a4fee712635fdfe894b6943
Denis Oliveira <denis.vetdev@gmail.com>
2024-06-26 20:22:09 -0300

    Update App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
   const [tasks, setTasks] = useState ([]);
@@ -26,17 +27,19 @@ const App = () => {
 
   return (
     <div>
-      <input value={newTask} onChange={e => setNewTask (e.target.value)}/>;
-      <button onClick={addTask}> Adicionar tarefa </button>;
+      <h1> Gerenciador de Tarefas </h1>
+      <input value={newTask} onChange={e => setNewTask (e.target.value)}/>
+      <button onClick={addTask}> Adicionar tarefa </button>
       <div>
-        <button onClick={() => setFilter('todas')}> Todas </button>;
-        <button onClick={() => setFilter('concluídas')}> Concluídas </button>;
-        <button onClick={() => setFilter('pendentes')}> Pendentes </button>; 
+        <h2> Filtros:</h2>
+        <button onClick={() => setFilter('todas')}> Todas </button>
+        <button onClick={() => setFilter('concluídas')}> Concluídas </button>
+        <button onClick={() => setFilter('pendentes')}> Pendentes </button> 
       </div>
       <ul>
         {filteredTasks.map((task, index) => (
           <li key={index}>
-            <input type="checkbox" checked={task.completed} onChange={() => toggleCompleted(index)}/>
+            <input type="checkbox" class="chekbox" checked={task.completed} onChange={() => toggleCompleted(index)}/>
             {task.name}
           </li>
         ))}
