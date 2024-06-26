7ac1b99f1ecb1eb960407174884adbd5ada21889
Denis Oliveira <denis.vetdev@gmail.com>
2024-06-26 19:19:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState ([]);
+  const [filter, setFilter] = useState ('todas');
+  const [newTask, setNewTask] = useState ('');
+  
+  const addTask = () => {
+    if(newTask) {
+      setTasks([...tasks, {name: newTask, completed: false}]);
+      setNewTask('');
+    }
+  }
+
+  const toggleCompleted = index => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  }
+
+  const filteredTasks = tasks.filter (task => {
+    if(filter === 'concluídas') return task.completed;
+    if(filter === 'pendentes') return !task.completed;
+    return true;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <input value={newTask} onChange={e => setNewTask (e.target.value)}/>;
+      <button onClick={addTask}> Adicionar tarefa </button>;
+      <div>
+        <button onClick={() => setFilter('todas')}> Todas </button>;
+        <button onClick={() => setFilter('concluídas')}> Concluídas </button>;
+        <button onClick={() => setFilter('pendentes')}> Pendentes </button>; 
+      </div>
+      <ul>
+        {filteredTasks.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={task.completed} onChange={() => toggleCompleted(index)}/>
+            {task.name}
+          </li>
+        ))}
+      </ul> 
+    </div>  
   );
-}
+};
 
 export default App;
